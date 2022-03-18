a05ef8cc7d956da6a2932242a2a92d3262628c11
Renato <emisor.tarea@gmail.com>
2022-03-18 16:29:51 -0600

    HotelSeeder

---
+++ backend/database/seeders/HotelSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\V1\Hotel\HEstado;
+use App\Models\V1\Hotel\HTipoCama;
+use Illuminate\Database\Seeder;
+
+class HotelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        HEstado::insert([
+            ['nombre' => 'DISPONIBLE'],
+            ['nombre' => 'MANTENIMIENTO'],
+            ['nombre' => 'INACTIVO']
+        ]);
+
+        HTipoCama::insert([
+            ['nombre' => 'Individual', 'cantidad' => 1],
+            ['nombre' => 'Matrimonial', 'cantidad' => 2],
+            ['nombre' => 'Queen', 'cantidad' => 2],
+            ['nombre' => 'King', 'cantidad' => 2]
+        ]);
+    }
+}
