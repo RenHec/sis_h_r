6c08cf5acf53c2208de44f5fbfb8999028de0e04
RenHec <emisor.tarea@gmail.com>
2022-04-18 09:44:35 -0600

    Formatenado fecha de logs

--- backend/app/Models/V1/Seguridad/Bitacora.php
+++ backend/app/Models/V1/Seguridad/Bitacora.php
@@ -42,6 +42,16 @@ class Bitacora extends Model
     ];
 
     /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
      * The accessors to append to the model's array form.
      *
      * @var array
