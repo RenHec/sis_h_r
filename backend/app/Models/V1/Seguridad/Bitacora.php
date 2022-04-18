4080be03df2b859d6b4381f79f2f9e257bc12fab
RenHec <emisor.tarea@gmail.com>
2022-04-18 09:33:33 -0600

    Formatenado fecha de logs

--- backend/app/Models/V1/Seguridad/Bitacora.php
+++ backend/app/Models/V1/Seguridad/Bitacora.php
@@ -47,8 +47,8 @@ class Bitacora extends Model
      * @var array
      */
     protected $casts = [
-        'created_at' => 'datetime:d-m-Y i:s',
-        'updated_at' => 'datetime:d-m-Y i:s'
+        'created_at' => 'datetime:d M Y H:i:s',
+        'updated_at' => 'datetime:d M Y H:i:s'
     ];
 
     /**
